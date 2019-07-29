e34e27f90cc66dde7b594ab115a3af73a1b95f95
Tomasz Rycerz <tomryc@gmail.com>
2019-07-29 23:27:53 +0100

    adding Primary Bean demo

--- src/main/java/guru/springframework/controllers/MyController.java
+++ src/main/java/guru/springframework/controllers/MyController.java
@@ -1,13 +1,21 @@
 package guru.springframework.controllers;
 
+import guru.springframework.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
+    private GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     public String hello(){
         System.out.println("Hello!");
 
-        return "foo";
+        return greetingService.sayGreeting();
     }
 }
